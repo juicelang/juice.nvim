24b38843e48557cc8f29bca7c4e7f27b2b2bc396
Jake Hamilton <jake.hamilton@hey.com>
2024-03-11 16:07:36 -0700

    fix: correct highlights

--- queries/juice/highlights.scm
+++ queries/juice/highlights.scm
@@ -1,22 +1,21 @@
+(hash_bang_line) @label
 (ERROR) @error
-(string) @string
-(string_content (escape_sequence) @string.escape)
-(string_content) @spell
 (bool_literal) @boolean
 (string_literal) @string
+(string_content) @spell
+(string_content (string_escape_sequence) @string.escape)
 (int_literal) @number
 (float_literal) @number
 (binary_literal) @number
 (octal_literal) @number
 (hex_literal) @number
-(string_content (string_escape_sequence) @string.escape)
 (comment) @comment
 "{" @punctuation.bracket
 "}" @punctuation.bracket
 "[" @punctuation.bracket
 "]" @punctuation.bracket
-"(" @punctuation.bracket
-")" @punctuation.bracket
+"(" @punctuation.paren
+")" @punctuation.paren
 
 (identifier) @label
 
@@ -44,6 +43,8 @@
 "fn" @keyword
 "type" @keyword
 "import" @keyword
+"loop" @keyword
+"for" @keyword
 
 (type_identifier) @type
 (macro_identifier) @macro
@@ -51,5 +52,4 @@
 (import_path) @label
 
 (function_declaration name: (identifier) @function)
-
 (("\"" @conceal) (#set! conceal ""))
