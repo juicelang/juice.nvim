793201a8d13e4fb5bb618d4cf36450b368717090
Jake Hamilton <jake.hamilton@hey.com>
2024-03-13 23:57:46 -0700

    feat: better highlighting

--- queries/juice/highlights.scm
+++ queries/juice/highlights.scm
@@ -1,60 +1,136 @@
-(hash_bang_line) @comment
 (ERROR) @error
+
+(hash_bang_line) @keyword.directive
+
 (bool_literal) @boolean
-(string_literal) @string
-(string_content) @spell
+(string_interpolation ["${" "}"] @punctuation.special) @none
+(string_literal [ "\"" ] @string) @none
+(string_text) @string @spell
 (string_content (string_escape_sequence) @string.escape)
-(int_literal) @number
-(float_literal) @number
-(binary_literal) @number
-(octal_literal) @number
-(hex_literal) @number
-(comment) @comment
-"{" @punctuation.bracket
-"}" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"(" @punctuation.paren
-")" @punctuation.paren
+(int_literal) @number.int
+(float_literal) @number.float
+(binary_literal) @number.binary
+(octal_literal) @number.octal
+(hex_literal) @number.hex
 
-"=" @operator
-":=" @operator
-"->" @operator
-"+" @operator
-"-" @operator
-"*" @operator
-"/" @operator
-">" @operator
-"<" @operator
-"==" @operator
-">=" @operator
-"<=" @operator
-"!=" @operator
-"&&" @operator
-"||" @operator
-"**" @operator
-"&" @operator
-"|" @operator
-"^" @operator
-"%" @operator
+(comment) @comment @spell
 
-"fn" @keyword
-"import" @keyword
-"export" @keyword
-"loop" @keyword
-"for" @keyword
-"match" @keyword
-"impl" @keyword
-"static" @keyword
-"foreign" @keyword
-"return" @keyword
-"break" @keyword
-"continue" @keyword
-"if" @keyword
-"else" @keyword
+((comment) @comment.documentation
+  (#lua-match? @comment.documentation "^///.*$"))
 
-(type_identifier) @type
-(macro_identifier) @macro
+(type_declaration name: (type_identifier) @local.definition.type (type_constructors (type_constructor_shorthand)))
+(type_declaration name: (type_identifier) @local.definition.namespace (type_constructors (type_constructor)))
+
+(variable_declaration name: (identifier) @local.definition.var)
+(variable_declaration name: (identifier) @local.definition.var)
+(type_declaration name: (type_identifier) @local.definition.type)
+
+((identifier) @constructor
+  (#eq? @constructor "new"))
+
+((identifier) @variable.builtin
+  (#eq? @variable.builtin "self"))
+
+(type_constructor constructor_name: (identifier) @constructor)
+(type_constructor (type_parameters (type_parameter name: (identifier)))) @variable.parameter
+
+(type_expression) @type
+(type_binary_expression operator: [ "&" "|" ] @operator) @operator
+
+(function_declaration) @local.scope
+(block) @local.scope
 
 (function_declaration name: (identifier) @function)
+(function_declaration name: (macro_identifier) @macro)
+
+(function_declaration params: (function_parameters (function_parameter param_name: (identifier)) ) @local.definition.parameter)
+(function_declaration params: (function_parameters (function_parameter param_name: (identifier)) ) @variable.parameter)
+
+(function_declaration params: (function_parameters (function_parameter param_type: (type_expression)) ) @type)
+
+(function_call name: (identifier) @function)
+(function_call name: (macro_identifier) @macro)
+
+[
+"{" 
+"}" 
+"[" 
+"]" 
+"(" 
+")" 
+] @punctuation.bracket
+
+[
+	"="
+	":="
+	"->"
+	"+"
+	"-"
+	"*"
+	"/"
+	">"
+	"<"
+	"=="
+	">="
+	"<="
+	"!="
+	"&&"
+	"||"
+	"**"
+	"&"
+	"|"
+	"^"
+	"%"
+	"!"
+	"?"
+] @operator
+
+[
+ ":"
+ ","
+ "."
+] @punctuation.delimiter
+
+[
+	"fn"
+	"match"
+	"impl"
+	"static"
+	"foreign"
+	"break"
+] @keyword
+
+[
+	"import"
+] @keyword.import
+
+[
+	"export"
+] @keyword.export
+
+[
+	"loop"
+	"for"
+	"continue"
+] @keyword.repeat
+
+[
+	"return"
+] @keyword.return
+
+[
+	"if"
+	"else"
+] @keyword.conditional
+
+(import_as [ "as" ] @keyword.import)
+(import_as (identifier) @module)
+
+(identifier) @variable
+
+(builtin_type) @type.builtin
+(type_identifier) @type
+
+(macro_identifier) @macro
+
 (("\"" @conceal) (#set! conceal ""))
